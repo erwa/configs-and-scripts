20ea12ca7e53c157558e1e6406f0ea6fcf2ec174
Anthony Hsu <erwaman@gmail.com>
2020-05-28 09:39:53 -0400

    added comments to git-rebase-i-2.sh

--- bin/git-rebase-i-2.sh
+++ bin/git-rebase-i-2.sh
@@ -2,12 +2,14 @@
 # https://stackoverflow.com/questions/2563632/how-can-i-merge-two-commits-into-one-if-i-already-started-rebase/48635637#48635637
 spawn git rebase -i HEAD~2
 
+# change the second "pick" to "squash"
 # down, delete word, insert 's' (for squash), Escape, save and quit
 send "jdwis \033:wq\r"
 
 expect "# This is a"
 
+# skip past first commit message (assumed to be one line), delete rest of file
 # down 4, delete 3 lines, save and quit
-send "4j3d\r:wq\r"
+send "4jdG\r:wq\r"
 
 interact
